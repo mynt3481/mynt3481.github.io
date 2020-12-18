a3ec0369ec5e7af1d61485136f661f318e91d09d
mynt3481 <muathangba23@gmail.com>
2020-12-18 15:13:22 +0700

    oki
    Done

---
+++ Prj2/Home.js
@@ -0,0 +1,34 @@
+﻿ $(function(){
+ new WOW().init();
+
+ $('.len').click(function(){
+ 	$('html,body').animate({scrollTop:0},500)
+ })
+
+ $(window).scroll(function(){
+ 	if($('body').scrollTop() >=500){
+ 		$('.len').addClass('lenvao')
+ 	}
+ 	else{
+ 		$('.len').removeClass('lenvao')
+ 	}
+
+ })
+
+ //**1sp.js**//
+ $('.duoi img').click(function(event) {
+ $('.duoi img').removeClass('chon');
+ $(this).addClass('chon');
+
+ anhto = $(this).attr('src');
+ $('.anhto').attr({'src':anhto});
+ });
+
+ 
+})
+
+
+
+
+
+ 
