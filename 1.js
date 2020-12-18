f54dc7e20164e0ad18b56c22e721c096886892ae
mynt3481 <muathangba23@gmail.com>
2020-12-18 15:00:56 +0700

    oki
    up

---
+++ 1.js
@@ -0,0 +1,18 @@
+﻿ $(function(){
+ 	new WOW().init();
+ 	$('.len').click(function(){
+ 	$('html,body').animate({scrollTop:0},500)
+ })
+
+
+ $(window).scroll(function(){
+ 	if($('body').scrollTop() >=390){
+ 		$('button.len').addClass('lenvao')
+ 	}
+ 	else{
+ 		$('button.len').removeClass('lenvao')
+ 	}
+
+ })
+})  
+ 
